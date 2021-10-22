81b3930998dcac3843e57977abd2fa5f8ae6d1ed
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-10-21 21:09:20 -0500

    Create question_2.sql

---
+++ question_2.sql
@@ -0,0 +1,13 @@
+SELECT
+    customer.customer_name,
+    AVG(invoice.total_price) AS average_price
+FROM
+    customer
+INNER JOIN
+    invoice
+    ON customer.id = invoice.customer_id
+WHERE
+    invoice.total_price <= (SELECT AVG(invoice.total_price) * 0.25 FROM invoice)
+GROUP BY
+    customer.customer_name,
+    invoice.total_price;
